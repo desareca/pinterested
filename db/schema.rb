6fa67d587904223ffd7528caa5feaf8447a8a6ee
desareca <carlos.saquel@gmail.com>
2021-10-16 18:50:28 -0300

    Capítulo V
    Construyendo Scaffold
    Agrega pins
    Modifica stilos de formularios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_16_022553) do
+ActiveRecord::Schema.define(version: 2021_10_16_205109) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
