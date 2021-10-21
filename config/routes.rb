2d7a3ef20310537f4e36640bebc1099bcb1be71c
desareca <carlos.saquel@gmail.com>
2021-10-20 22:37:09 -0300

    Capitulo VII
    Agrega estilo con Masonry (bootstrap 4)
    Agrega paginación (con Kaminiri)
    Agrega campo Name a tabla User

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :pins
   devise_for :users
   root 'pins#index'
-  get 'home/about'
+  get 'home/about' => 'home#about'
+  get 'home' => 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
